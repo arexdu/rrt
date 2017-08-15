3b3395c8d071045daf3d1c50ebfce1d2b43845f4
arexdu <710776009@qq.com>
2017-08-15 22:05:41 +0800

    map filter test

--- app/mapFilterTest.js
+++ app/mapFilterTest.js
@@ -2,6 +2,6 @@ export const mapFilterTest = () => {
     let values = [34, 23, 76, 3, 9, 98, 100];
     let valuesMap = values.map((n) => n*10);
     console.log(valuesMap);
-    let valuesFilter = values.filter((n) => {return n > 30});
+    let valuesFilter = values.filter((n) => {return n>30}).map((n) => n*10);
     console.log(valuesFilter);
 };
