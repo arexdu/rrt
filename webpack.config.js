27eee7ac674b2b29907db8f72dee98883a6dcdcd
arexdu <710776009@qq.com>
2017-08-15 08:51:00 +0800

    add babel to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,18 @@ module.exports = {
     output: {
         path: __dirname + '/build',
         filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/
+            }
+        ]
+    },
+    devServer: {
+        port: 3000,
+        contentBase: __dirname + '/build',
+        inline: true
     }
 }
